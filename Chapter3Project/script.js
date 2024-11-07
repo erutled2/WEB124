696dbc6fdd96f3a10df573bb338fb364bf41d1dd
erutled2 <erutled2@stumail.jccc.edu>
2024-11-06 19:31:55 -0600

    Completed Project 3

---
+++ Chapter3Project/script.js
@@ -0,0 +1,14 @@
+// Eric Rutledge 10/21/2024
+
+let fullName = "Eric Daniel Rutledge";
+console.log(fullName);
+let desiredAnnualSalary = 200000;
+console.log(desiredAnnualSalary);
+let isVeteran = false;
+console.log(isVeteran);
+let threeFriends = ["Cynthia", "Braden", "Skye"];
+console.log(threeFriends);
+let desiredFriendSalaries = [100000, 150000, 125000];
+console.log(desiredFriendSalaries);
+let aFriend = {firstName: "Dylan", lastName: "Ryder", desiredSalary: 100000};
+console.log(aFriend);
